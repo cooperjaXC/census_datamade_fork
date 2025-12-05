132589eaa005acf26099df2b9b725059610eba65
W. Nick Pappin <nick.pappin@wsu.edu>
2025-12-05 11:17:53 -0800

    Adding support for 2024 to ACS1 data.

--- census/core.py
+++ census/core.py
@@ -422,10 +422,10 @@ class ACS3DpClient(ACS3Client):
 
 class ACS1Client(ACSClient):
 
-    default_year = 2023
+    default_year = 2024
     dataset = 'acs1'
 
-    years = (2023, 2022, 2021, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005)
+    years = (2024, 2023, 2022, 2021, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005)
 
     @supported_years()
     def state_county_subdivision(self, fields, state_fips,
